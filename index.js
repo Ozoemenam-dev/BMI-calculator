59b48ef025170408b7c24de9db15bade3adb0e93
Okpara Chidiebere Ozoemenam <114841298+Ozoemenam-dev@users.noreply.github.com>
2022-10-16 20:04:12 +0000

    Bmi Claculator

--- index.js
+++ index.js
@@ -1,28 +1,7 @@
-//variable declaration
-// let fullName = "Brandon \"Anderson\"";
-// console.log(fullName)
 
-// let size = 14;
-// size = size.toString();
-// console.log(typeof size);
-
-// let size = "14";
-// size = Number.parseInt("14");
-// console.log(size)
-// console.log(typeof size)
-
-// let name = {
-//   firstName: "john",
-//   lastName: "cena"
-// }
-
-// console.log(name.firstName)
-
-// let zuri = ["mark", "james", "cole","matter"]
-// console.log(Math.floor(Math.random() * zuri.length))
 
 function bmiCalculator(weight, height) {
   var bmi = Math.floor(weight / Math.pow(height, 2));
   return bmi;
 }
-console.log(bmiCalculator(65, 1.8))
+console.log("Your Body Mass Index is " + bmiCalculator(65, 1.8))
